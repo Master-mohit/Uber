145bdfbd2218e299c0067ce3a020684d6c9fd483
Master-mohit <mohitmansigupta9098@gmail.com>
2025-01-17 01:20:33 +0530

    completed user logdin

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -7,6 +7,16 @@ router.post('/register', [
     body('email').isEmail().withMessage('Invalid email'),
     body('firstname').isLength({ min: 3 }).withMessage('First name must be at least 3 characters long'),
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
-], userController.registerUser);
+], userController.registerUser
+);
+
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+],
+    userController.loginUser
+)
+
+
 
 module.exports = router;
