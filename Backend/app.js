ac311d893330b2162f84b3c393004fa63d9f2802
Master-mohit <mohitmansigupta9098@gmail.com>
2025-01-17 01:16:23 +0530

    done user logdin

--- Backend/app.js
+++ Backend/app.js
@@ -4,13 +4,19 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.route');
 
 connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send("hello world")
-})
+});
+
+app.use('./user', userRoutes);
+
 
 module.exports=app;
---
+++ Backend/controllers/user.controller.js
@@ -0,0 +1,28 @@
+const userModel = require('../model/user.model');
+const userService = require('../survices/user.service');
+const { validationResult } = require('express-validator');
+
+module.exports.registerUser = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { firstname, lastname, email, password } = req.body;
+    const hashedPassword = await userModel.hashPassword(password);
+
+    try {
+        const user = await userService.createUser({
+            firstname,
+            lastname,
+            email,
+            password: hashedPassword
+        });
+
+        const token = user.generateAuthToken();
+
+        res.status(201).json({ token, user });
+    } catch (error) {
+        next(error);
+    }
+};
